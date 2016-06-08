ef39fe85af875c0d39696d0408e55a93634709b0
Évelyne Lachance <eslachance@gmail.com>
2016-06-07 22:35:52 -0400

    delete echo message. Because, why not.

--- commands/util/echo.js
+++ commands/util/echo.js
@@ -5,6 +5,7 @@ const config    = require(Constants.Util.CONFIG);
 
 const echo = new Command('Echoes any text, as a test!', '', 2, null, (bot, msg, suffix) => {
 	bot.reply(msg, msg.content.substr(msg.content.indexOf(' ')+1));
+	bot.deleteMessage(msg);
 });
 
 module.exports = echo;
